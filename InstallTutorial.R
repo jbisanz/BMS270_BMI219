7b1d293f0952b8930474b374b50c5d6afae21f00
Jordan Bisanz <jbisanz@WPEDC0YC0R1.ucsf.edu>
2017-05-08 20:31:40 -0700

    Link for Windows Users

--- InstallTutorial.R
+++ InstallTutorial.R
@@ -126,5 +126,5 @@ for (i in 1:nrow(metadata)){
   }
 }
 } else {
-    print("Sequencing data could not be automatically downloaded. Please download from the following link:")
+    print("Sequencing data could not be automatically downloaded. Please download from the following link: https://github.com/jbisanz/BMS270_BMI219/raw/master/tutorialdata.tar.gz")
   }
